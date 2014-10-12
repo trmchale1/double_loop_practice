a85e408de1df0ddb071b2941484e26f60cf5b866
Tim <trmchale@gmail.com>
2014-10-12 03:29:59 -0400

    first commit

---
+++ doubleLoop.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	for (int k = 0; k < 10; k++)
+	{
+
+		for (int j =0; j < k; j++)
+		{
+
+			cout << "k = " << k << " j = " << j << endl;
+
+		}
+
+
+	}
+
+
+
+
+}
